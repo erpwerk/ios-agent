e5c9077d8cb453057b25bbe44abe264637ec5079
erpwerk <erpwerk09@gmail.com>
2026-03-11 10:19:53 +0100

    add files via upload
    uploading the new project

---
+++ ios-app/Sources/WebView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let configuration = WKWebViewConfiguration()
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.allowsBackForwardNavigationGestures = true
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
